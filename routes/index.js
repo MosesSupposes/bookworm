8361494fa8124ec5f3268adf6f63b3b049154549
Moses Samuel <mosamuel98@gmail.com>
2017-07-14 16:10:52 -0700

    add GET and POST routes for registration form

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+
 // GET /
-router.get('/', function(req, res, next) {
-  return res.render('index', { title: 'Home' });
+router.get('/', (req, res, next) => {
+    return res.render('index', { title: 'Home' });
+});
+
+// GET /register
+router.get('/register', (req, res, next) => {
+    return res.render('register', { title: 'Sign Up' });
+});
+
+// POST /register
+router.post('/register', (req, res, next) => {
+    return res.send('User created!');
 });
 
 // GET /about
-router.get('/about', function(req, res, next) {
-  return res.render('about', { title: 'About' });
+router.get('/about', (req, res, next) => {
+    return res.render('about', { title: 'About' });
 });
 
 // GET /contact
-router.get('/contact', function(req, res, next) {
-  return res.render('contact', { title: 'Contact' });
+router.get('/contact', (req, res, next) => {
+    return res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
